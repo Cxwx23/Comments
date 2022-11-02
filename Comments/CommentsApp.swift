15355a9ffba27b8332c044bbe752dcffc32d647e
Cole Warner <cwarner13@mail.csuchico.edu>
2022-11-02 13:28:49 -0700

    Initial Commit

---
+++ Comments/CommentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentsApp.swift
+//  Comments
+//
+//  Created by Cole Warner on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CommentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
